da4d11943fe0acd67f9402ff034917c3c260dc69
MuhammadUsmanAmjad <l215777@lhr.nu.edu.pk>
2023-12-05 08:25:00 +0500

    added testfiles for about controller and contact controller the new funcionalities.

---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/controllerTests/AboutControllerTest.java
@@ -0,0 +1,15 @@
+package com.jtspringproject.JtSpringProject.controller;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import com.jtspringproject.JtSpringProject.controller.AboutController;
+
+public class AboutControllerTest {
+
+    @Test
+    public void testShowAboutPage() {
+        AboutController aboutController = new AboutController();
+        String viewName = aboutController.showAboutPage();
+        assertEquals("about", viewName);
+    }
+}
+
---
+++ JtProject/src/test/java/com/jtspringproject/JtSpringProject/controllerTests/ContactControllerTest.java
@@ -0,0 +1,15 @@
+package com.jtspringproject.JtSpringProject.controller;
+
+import com.jtspringproject.JtSpringProject.controller.ContactController;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class ContactControllerTest {
+
+    @Test
+    public void testShowContactPage() {
+        ContactController contactController = new ContactController();
+        String viewName = contactController.showContactPage();
+        assertEquals("Contact", viewName);
+    }
+}
